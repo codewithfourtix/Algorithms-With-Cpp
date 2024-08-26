a4e223a7e7ed2479968408dd565949f04114692f
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-26 15:20:07 +0500

    algo no 1

---
+++ algo1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int reverseFunc(int arr[], int sizeOfArray)
+{
+  int start = 0;
+  int end = sizeOfArray - 1;
+  while (start < end)
+  {
+    swap(arr[start], arr[end]);
+    start++;
+    end--;
+  }
+}
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5};
+  int sizeOfArray = sizeof(arr) / sizeof(arr[0]);
+  reverseFunc(arr, sizeOfArray);
+  for (int i = 0; i < sizeOfArray; i++)
+  {
+    cout << arr[i] << endl;
+  }
+  return 0;
+}
