42fceb9c5fc8c731367dad13b79ea4a8bfe86fb1
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-09-01 22:17:01 +0500

    title added

---
+++ algo6.cpp
@@ -0,0 +1,17 @@
+// Simple interest
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float principal, rate, time, interest;
+  cout << "Enter principal amount: ";
+  cin >> principal;
+  cout << "Enter rate of interest: ";
+  cin >> rate;
+  cout << "Enter time (in years): ";
+  cin >> time;
+  interest = (principal * rate * time) / 100;
+  cout << "Simple Interest: " << interest << endl;
+  return 0;
+}
