1f8903d0b1dde671626187ccf88a943ba97745d0
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-09-02 20:53:06 +0500

    code

---
+++ algo7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cout << "Enter three numbers: ";
+  cin >> a >> b >> c;
+  int largest = a;
+  if (b > largest)
+    largest = b;
+  if (c > largest)
+    largest = c;
+  cout << "The largest number is: " << largest << endl;
+  return 0;
+}
