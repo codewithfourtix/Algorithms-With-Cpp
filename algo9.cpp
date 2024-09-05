3419a426abbd8402aa1431034bab86d9be8f94e5
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-09-05 20:58:40 +0500

    Coded

---
+++ algo9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPrimeDigit(int digit)
+{
+  return (digit == 2 || digit == 3 || digit == 5 || digit == 7);
+}
+
+int main()
+{
+  int number, sum = 0;
+  cout << "Enter a positive integer: ";
+  cin >> number;
+
+  while (number > 0)
+  {
+    int digit = number % 10;
+    if (isPrimeDigit(digit))
+    {
+      sum += digit;
+    }
+    number /= 10;
+  }
+
+  cout << "Sum of prime digits: " << sum << endl;
+  return 0;
+}
